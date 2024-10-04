c1173867dc76631e3852a81292602239f53db6eb
muja07001 <muja07001@E-PW05CGR9>
2024-10-04 15:36:23 +0200

    Add project files.

---
+++ inlämning 2.4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inlämning_2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ange lön för den första anställda");
+            double lön1= Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("ange lön för den andra anställda ");
+            double lön2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("ange lön för den tredje anställd");
+            double lön3 = Convert.ToDouble(Console.ReadLine());
+
+            double medellön = (lön1 + lön2 + lön3) / 3;
+
+            Console.WriteLine($"medellön för de anställda är: {medellön} kr"); 
+
+
+        }
+    }
+}
---
+++ inlämning 2.4/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("inlämning 2.4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("IT-avdelningen")]
+[assembly: AssemblyProduct("inlämning 2.4")]
+[assembly: AssemblyCopyright("Copyright © IT-avdelningen 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7f6d1b77-4e44-4aa7-b235-ac7149ece5f6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
